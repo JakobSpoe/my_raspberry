a15f181cbddabd06722476b203fbf8dca4caf142
JakobSode <jakob.soderberg@hotmail.com>
2024-02-14 10:18:32 +0100

    fix

--- humidity/humidity.py
+++ humidity/humidity.py
@@ -5,12 +5,10 @@ sense = SenseHat()
 sense.set_rotation(180)
 red = (255, 0, 0)
 
-
-humidity = sense.get_humidity().__round__(2)
-
-temperature = sense.get_temperature_from_humidity().__round__(2)
+# temperature = sense.get_temperature_from_humidity().__round__(2)
 
 while True:
+  humidity = sense.get_humidity().__round__(2)
   sense.show_message("Humidity: %s" % humidity, text_colour=red)
   time.sleep(15)
 # sense.show_message("Temperature: %s C" % temperature, text_colour=red)
